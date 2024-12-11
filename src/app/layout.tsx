50a32500f95e225843e96e299039844be8c79900
Nikita Loginov <nikitaloginov264@gmail.com>
2024-12-11 16:33:07 +0300

    fix графика

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import StyledComponentsRegistry from './registry';
-import { Header, Footer } from '@/components/widgets';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
