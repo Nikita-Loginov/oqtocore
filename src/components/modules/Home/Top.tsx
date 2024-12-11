f5d59e91084a41b2ad106df3c98a0d892f77f7b2
Nikita Loginov <nikitaloginov264@gmail.com>
2024-12-11 14:05:36 +0300

    проба без планеты

--- src/components/modules/Home/Top.tsx
+++ src/components/modules/Home/Top.tsx
@@ -72,7 +72,7 @@ export default function Top() {
 
                 <TopGallaxy>
                     {/* <canvas className='webgl'></canvas> */}
-                    <Gallaxy />
+                    {/* <Gallaxy /> */}
                 </TopGallaxy>
             </Container>
         </TopBox>
