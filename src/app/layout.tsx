a3b4fb93e9b8e1371faac0b733f80839a20f5b0c
Nikita Loginov <nikitaloginov264@gmail.com>
2024-12-11 15:09:13 +0300

    fix скачка стилей

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,142 +1,15 @@
 'use client';
-import { createGlobalStyle } from 'styled-components';
-
+import StyledComponentsRegistry from './registry';
 import { Header, Footer } from '@/components/widgets';
 
-const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: 'Eina';
-        src: url('/fonts/EinaRegular.woff2') format('woff2');
-        font-weight: 400;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Eina';
-        src: url('/fonts/EinaSemiBold.woff2') format('woff2');
-        font-weight: 600;
-        font-style: normal;
-    }
-
-    * {
-        padding: 0;
-        margin: 0;
-        border: 0;
-        box-sizing: border-box;
-    }
-
-    html {
-        scroll-behavior: smooth;
-        overflow-x: hidden;
-    }
-
-    *,
-    *:before,
-    *:after {
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-    :focus,
-    :active {
-        outline: none;
-    }
-    a:focus,
-    a:active {
-        outline: none;
-    }
-
-    nav,
-    footer,
-    header,
-    aside {
-        display: block;
-    }
-
-    input,
-    button,
-    textarea {
-        font-family: inherit;
-    }
-
-    input::-ms-clear {
-        display: none;
-    }
-    button {
-        cursor: pointer;
-    }
-    button::-moz-focus-inner {
-        padding: 0;
-        border: 0;
-    }
-    a,
-    a:visited {
-        text-decoration: none;
-    }
-    a:hover {
-        text-decoration: none;
-    }
-    ul li {
-        list-style: none;
-    }
-    img {
-        vertical-align: top;
-    }
-
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-        font-size: inherit;
-        font-weight: 400;
-    }
-
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    input[type='number'] {
-        -moz-appearance: textfield;
-    }
-
-    body {
-        font-family: 'Eina', sans-serif;
-        overflow-x: hidden;
-        background-color: #010101;
-        color: #fff;
-        font-size: 14px;
-        line-height: 1.35;
-        font-weight: 400;
-    }
-`;
-
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <GlobalStyle />
-            
-            <html
-                lang='en'
-                suppressHydrationWarning
-            >
-                <body>
-                    <Header />
+        <html>
+            <body>
 
-                    <main>
-                        {children}
-                    </main>
+                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
 
-                    <Footer />
-                </body>
-            </html>
-        </>
+            </body>
+        </html>
     );
 }
