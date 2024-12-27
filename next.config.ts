c4b28062b1d3caf379b3e2a4169730a1f000a3bb
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2024-12-27 12:51:44 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,32 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: false,
     compiler: {
-        styledComponents: true,
-      },
+      styledComponents: true,
+    },
+    webpack(config, { isServer }) {
+      config.module.rules.push({
+        test: /\.glsl$/,
+        use: 'raw-loader',
+      });
+
+      if (!isServer) {
+        config.optimization.splitChunks = {
+          chunks: 'all', // Разделение бандлов по файлам
+          // minSize: 50000,
+          // maxSize: 200000,// Разделить файлы, превышающие 200 КБ
+        };
+  
+        // Убираем ненужные логи
+        config.performance = {
+          hints: false,
+        };
+  
+        config.optimization.minimize = true;
+        config.optimization.concatenateModules = true;
+
+      }
+      return config;
+    },
 };
 
 export default nextConfig;
