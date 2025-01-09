d6efc2ef4776e1bad55ce0aba55d0fbd2c52e2e3
nikita <nikitaloginov264@gmail.com>
2025-01-09 14:41:00 +0300

    fix для build-3.....

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -3,4 +3,4 @@ export { Footer } from './Footer/Footer';
 export { Services } from './Services/Services';
 export { Container } from './Container/Container';
 export { Stories } from './Stories/Stories';
-export { Skeleton } from './Skeleton/Skeleton';
+// export { Skeleton } from './Skeleton/Skeleton';
