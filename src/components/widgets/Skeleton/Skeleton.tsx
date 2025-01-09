5595ad0a9aea4334a46ed9470ae68dac7f46cbd1
nikita <nikitaloginov264@gmail.com>
2025-01-09 14:39:01 +0300

    fix для build-2.....

--- src/components/widgets/Skeleton/Skeleton.tsx
+++ src/components/widgets/Skeleton/Skeleton.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+// import React from 'react';
+// import ContentLoader from 'react-content-loader';
 
-export const Skeleton: React.FC = (props) => (
-    <ContentLoader
-        speed={2}
-        width={"100%"}
-        height={250}
-        viewBox='0 0 100% 250'
-        backgroundColor="rgb(243, 243, 243)"
-        foregroundColor="rgb(187, 187, 187)"
-        {...props}
-    >
-        <rect
-            x='0'
-            y='0'
-            rx='0'
-            ry='0'
-            width='100%'
-            height='155'
-        />
-        <rect
-            x='0'
-            y='205'
-            rx='0'
-            ry='0'
-            width='100%'
-            height='37'
-        />
-        <rect
-            x='0'
-            y='170'
-            rx='0'
-            ry='0'
-            width='100%'
-            height='20'
-        />
-    </ContentLoader>
-);
+// export const Skeleton: React.FC = (props) => (
+//     <ContentLoader
+//         speed={2}
+//         width={"100%"}
+//         height={250}
+//         viewBox='0 0 100% 250'
+//         backgroundColor="rgb(243, 243, 243)"
+//         foregroundColor="rgb(187, 187, 187)"
+//         {...props}
+//     >
+//         <rect
+//             x='0'
+//             y='0'
+//             rx='0'
+//             ry='0'
+//             width='100%'
+//             height='155'
+//         />
+//         <rect
+//             x='0'
+//             y='205'
+//             rx='0'
+//             ry='0'
+//             width='100%'
+//             height='37'
+//         />
+//         <rect
+//             x='0'
+//             y='170'
+//             rx='0'
+//             ry='0'
+//             width='100%'
+//             height='20'
+//         />
+//     </ContentLoader>
+// );
 
